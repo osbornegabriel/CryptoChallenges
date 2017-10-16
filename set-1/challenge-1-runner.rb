84e4508f27470ebfa42abd6e0f10350b04b9a913
Gabriel Osborne <osbornegabriel@gmail.com>
2017-10-16 13:40:07 -0500

    Write from_bin and to_bin decoder methods, finish solving challenge 1

--- set-1/challenge-1-runner.rb
+++ set-1/challenge-1-runner.rb
@@ -13,16 +13,8 @@ require_relative 'challenge-1'
 
 starting_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-hex = Decoder.new(4, (0..9).to_a + ('a'..'f').to_a)
-base64 = Decoder.new(6, ('A'..'Z').to_a + ('a'..'z').to_a + ('1'..'9').to_a + ['+'] + ['/'])
+hex = Decoder.new(4, ('0'..'9').to_a + ('a'..'f').to_a)
+base64 = Decoder.new(6, ('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a + ['+'] + ['/'])
 
-p hex.bin_rep_length
-p hex.to_base10
-p hex.from_base10
-
-p base64.bin_rep_length
-p base64.to_base10
-p base64.from_base10
-
-# p starting_string
-# p system_to_binary(starting_string, hex_base10_table)
+hex_bin = hex.to_bin(starting_string)
+p base64.from_bin(hex_bin)
--- set-1/challenge-1.rb
+++ set-1/challenge-1.rb
@@ -1,10 +1,15 @@
 class Decoder
-  attr_accessor :bin_rep_length, :to_base10, :from_base10
+  attr_accessor :bin_rep_length, :from_bin, :to_bin
 
   def initialize(bin_rep_length, values_table)
     @bin_rep_length = bin_rep_length
+    @bit_vals = set_bit_vals
     @to_base10 = create_to_base10(values_table)
     @from_base10 = create_from_base10
+  end
+
+  def set_bit_vals
+    [128,64,32,16,8,4,2,1].last(@bin_rep_length)
   end
 
   def create_to_base10(table)
@@ -15,20 +20,38 @@ class Decoder
   def create_from_base10
     @to_base10.invert
   end
+
+  def find_bits(base10_num)
+    bits = @bit_vals.map do |b|
+      if base10_num >= b
+        base10_num -= b
+        '1'
+      else
+        '0'
+      end
+    end
+    bits.join
+  end
+
+  def from_bits(bin_num)
+    value = 0
+    @bit_vals.each_with_index do |v,i|
+      value += v if bin_num[i] == '1'
+    end
+    value
+  end
+
+  def from_bin(num_string)
+    bin_vals = num_string.scan(eval("/.{#{@bin_rep_length}}/"))
+    base10_vals = bin_vals.map{|bin_val| from_bits(bin_val)}
+    base10_vals.map!{|integer| @from_base10[integer]}
+    base10_vals.join
+  end
+
+  def to_bin(num_string)
+    num_chars = num_string.chars
+    num_chars.map!{|char| @to_base10[char]}
+    num_chars.map!{|char| find_bits(char)}
+    num_chars.join
+  end
 end
-
-# def system_to_binary(sys_string, system_conversion_table)
-#   values = sys_string.scan(/../)
-#   values.map{}
-#   # sys_string.gsub(/./, hex_base10_table)
-# end
-
-# def binary_to_system
-
-# end
-
-# def hex_base10_table
-#   hex_vals = (0..9).to_a + ('a'..'f').to_a
-#   base10_vals = Array(0..15)
-#   hex_vals.zip(base10_vals).to_h
-# end
