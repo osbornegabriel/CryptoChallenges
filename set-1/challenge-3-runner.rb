0ba7fa74b60a1aa923613882cb4ab25d78f35619
Gabriel Osborne <osbornegabriel@gmail.com>
2017-10-17 14:36:06 -0500

    update to finding blankspace with find_cypher method. Build conversion table between hexadecimal and ascii plaintext. Solved this challenge on a basic level, now time to refactor

--- set-1/challenge-3-runner.rb
+++ set-1/challenge-3-runner.rb
@@ -1,1 +1,37 @@
 require_relative 'challenge-3'
+
+=begin
+
+  Single-byte XOR cipher
+  The hex encoded string:
+
+  1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+  ... has been XOR'd against a single character. Find the key, decrypt the message.
+
+  You can do this by hand. But don't: write code to do it for you.
+
+  How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric. Evaluate each output and choose the one with the best score.
+
+  Achievement Unlocked
+  You now have our permission to make "ETAOIN SHRDLU" jokes on Twitter.
+
+=end
+
+hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+p bin_string = HEX.to_bin(hex_string)
+
+p pairings = count_values(2, hex_string)
+p keys = pairings.keys
+cyphers = keys.map do |key|
+  cypher = find_xor(key)
+end
+new_bin_strings = []
+cyphers.each{|cypher| new_bin_strings << xor_cypher(cypher, bin_string)}
+new_bin_strings.map!{|string| HEX.from_bin(string)}
+p new_bin_strings[-1]
+p new_bin_strings[-1].gsub(/../, HEX_TO_TEXT)
+
+
+# cypher = p find_xor(last_key)
+# p new_bin_string = xor_cypher(cypher, bin_string)
+# p HEX.from_bin(new_bin_string)
--- set-1/challenge-3.rb
+++ set-1/challenge-3.rb
@@ -6,3 +6,19 @@ def count_values(scan_size, string)
   value_count = uniq_vals.map!{|val| [val, collection.count(val)]}
   value_count.sort_by{|key, value| value}.to_h
 end
+
+def find_xor(hex_pair)
+  binary_blankspace = '00100000'
+  # binary_letter_e = '01100101'
+  binary_of_hex_pair = HEX.to_bin(hex_pair)
+  xor(binary_blankspace, binary_of_hex_pair)
+  # xor(binary_letter_e, binary_of_hex_pair)
+end
+
+def xor_cypher(cypher, bin_string)
+  binary_sets = bin_string.scan(/.{8}/)
+  binary_sets.map!{|set| xor(set, cypher)}
+  binary_sets.join
+end
+
+HEX_TO_TEXT = ((65..90).to_a.map{|v| v.to_s(16)}.zip('A'..'Z') + (97..122).to_a.map{|v| v.to_s(16)}.zip('a'..'z') + [['20', ' ']]).to_h
