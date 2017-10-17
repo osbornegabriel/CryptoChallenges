09395e5256dd3f3273a3533c5570cc7f18e3e100
Gabriel Osborne <osbornegabriel@gmail.com>
2017-10-17 16:14:56 -0500

    Create challenge4 runner, code, and txt file required to be parsed

---
+++ set-1/challenge-4-runner.rb
@@ -0,0 +1,9 @@
+require_relative 'challenge-4'
+
+=begin
+  One of the 60-character strings in THIS FILE(linked) has been encrypted by single-character XOR.
+
+  Find it.
+
+  (Your code from #3 should help.)
+=end
