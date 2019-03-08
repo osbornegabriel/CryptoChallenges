4d10674565cdfc3cae8850a20e9ac2bba269736f
Gabriel Osborne <osbornegabriel@gmail.com>
2019-03-08 11:38:00 -0500

    Write new base-10 or method

--- set-1/challenge-2.rb
+++ set-1/challenge-2.rb
@@ -1,5 +1,16 @@
 require_relative 'challenge-1'
 
+# New fun method, should you wish to use a base 10 str instead of binary for a future xor:
+
+def xor_digits(str_one, str_two)
+  xor_str = ''
+  str_one.each_with_index |str,i|
+    xor_str << (str[i].to_i ^ str_two[i].to_i).to_s
+  end
+end
+
+## Original Solution
+
 def xor(bin_str_one, bin_str_two)
   new_bin_str = ''
   bin_str_one.length.times {|i| bin_str_one[i] == bin_str_two[i] ? new_bin_str << '0' : new_bin_str << '1'}
